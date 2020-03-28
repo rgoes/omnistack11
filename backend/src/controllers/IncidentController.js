01f90a4edc09881d794b74190a2b610b7a19d690
Rafael Goes Turchenski <rafaagoes@gmail.com>
2020-03-28 19:28:53 -0300

    Mobile da aplicação

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -16,7 +16,7 @@ module.exports = {
     const [count] = await connection('incidents').count();
     console.log(count)
     response.header('X-Total-Count', count['count(*)']);
-    const incidents = await connection('incidents').join('ongs', 'ongs.id', '=', 'incidents.ong_id').limit(5).offset((page - 1)*5).select(['incidents.*', 'ongs.name', 'ongs.email', 'ongs.whatsapp', 'ongs.uf']);
+    const incidents = await connection('incidents').join('ongs', 'ongs.id', '=', 'incidents.ong_id').limit(5).offset((page - 1)*5).select(['incidents.*', 'ongs.name', 'ongs.email', 'ongs.whatsapp', 'ongs.city','ongs.uf']);
     return response.json(incidents);
   },
 
